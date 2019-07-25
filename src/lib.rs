62fe185a9eb6ab2d758b2a5d7e31f35c27ac41b7
Diego Braga <dsbrgg@gmail.com>
2019-07-25 22:34:40 +0100

    move logic to Locker and clean main

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
+use std::io::stdin;
 use std::io::Write;
 use std::io::Read;
 
 use std::fs::File;
 use std::path::Path;
+
+enum LockerAction {
+    Read,
+    Write
+}
 
 #[derive(Debug)]
 pub struct Locker<'a> {
@@ -10,25 +16,45 @@ pub struct Locker<'a> {
 }
 
 impl<'a> Locker<'a> {
+
     pub fn new() -> Locker<'a> {
         Locker { 
             path: Some("t.txt"), 
         }
     }
 
-    pub fn read(&self) {
-        let path = if let Some(path) = &self.path {
-            Path::new(path)
-        } else {
-            panic!("Locker can't be read if not instantianed by new method!");
-        };
+    fn open(&self, action: LockerAction) -> File {
+        let unwrap = &self.path.unwrap();
+        let path = Path::new(&unwrap);
         
-        let mut contents = String::new();
-        let mut open = File::open(&path)
-            .expect("Unable to open locker!");
+        let file = match action {
+            LockerAction::Read => File::open(&path).expect("Unable to open locker to read!"),
+            LockerAction::Write => File::create(&path).expect("Unable to open locker to write!"),
+        };
 
-        open.read_to_string(&mut contents)
+        file
+    }
+
+    pub fn read(&self) {
+        let mut file = &self.open(LockerAction::Read);
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents)
             .expect("Unable to read opened locker!");
+    }
+
+    pub fn write(&self) {
+        let mut file = &self.open(LockerAction::Write);
+        let mut input = String::new();
+
+        stdin()
+            .read_line(&mut input)
+            .expect("Failed to read user input");
+
+        match file.write_all(input.as_bytes()) {
+            Err(why) => panic!("Couldn't write to locker: {}", why),
+            Ok(_) => println!("Success writing to locker!")
+        } 
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,6 @@
 use rk::Locker;
 
-use std::io::stdin;
-
 fn main() {
     let lock = Locker::new();
-
-    lock.read();
-
-    // let mut input = String::new();
-
-    // stdin()
-    //     .read_line(&mut input)
-    //     .expect("Failed to read line");
-    // 
-    // let path = Path::new("t.txt");
-    // let display = path.display();
-
-    // let mut file = match File::create(&path) {
-    //     Err(why) => panic!("couldn't create {} : {}", display, why),
-    //     Ok(file) => file,
-    // };
-
-    // match file.write_all(input.as_bytes()) {
-    //     Err(why) => panic!("couldn't write to {} : {}", display, why),
-    //     Ok(_) => println!("success {}", display),
-    // };
-
-    // let mut read_file = File::open(&path).expect("Unable to open file");
-    // let mut contents = String::new();
-
-    // read_file.read_to_string(&mut contents).expect("Unable to read file");
-
-    // println!("reding: {}", contents);
+    lock.write();
 }
