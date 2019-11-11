4fe184865ce71d3ea30d9a1c9a81aadc33422d82
dsbrgg <dsbrgg@gmail.com>
2019-11-11 18:30:40 +0000

    Update manager trait with associated type

--- src/managers/traits.rs
+++ src/managers/traits.rs
@@ -2,13 +2,15 @@ use std::io;
 use std::path::{PathBuf};
 
 pub trait Manager {
+    type Output;
+
     fn init(&mut self) -> io::Result<()>;
 
     fn create(&mut self, path: &str) -> io::Result<()>;
 
     fn remove(&mut self, path: &str) -> io::Result<()>;
 
-    fn read(&mut self, path: &str) -> io::Result<Vec<String>>;
+    fn read(&mut self, path: &str) -> io::Result<Self::Output>;
 
     fn pb_to_str(path: &PathBuf) -> String {
         path
