41d598452ae360cc1be817cb61aa6fa12d3aaa4a
dsbrgg <dsbrgg@gmail.com>
2019-11-25 17:42:26 +0000

    change method used in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use std::path::{PathBuf};
 
 use locker::Locker;
 
-use managers::traits::Manager;
+use managers::manager::Manager;
 use managers::dir_manager::DirManager;
 use managers::file_manager::FileManager;
 
@@ -68,7 +68,7 @@ impl Keeper {
         }
 
         if let Some(e) = entity { 
-            self.directories.create(e)?; 
+            self.directories.create_locker(e)?; 
         }
 
         if let Some(a) = account {
@@ -77,7 +77,7 @@ impl Keeper {
             let e = entity.unwrap();
             let p = DirManager::append_paths(e, &paths);
 
-            self.directories.create(&p)?; 
+            self.directories.create_locker(&p)?; 
         } 
 
         if let Some(p) = password {
@@ -86,7 +86,7 @@ impl Keeper {
             let e = entity.unwrap();
             let p = FileManager::append_paths(e, &paths);
 
-            self.files.create(&p)?; 
+            self.files.create_locker(&p)?; 
         }
 
         Ok(Resolve::Add)
@@ -107,7 +107,7 @@ impl Keeper {
         let a = account.unwrap_or("");
         let ap = DirManager::append_path(&e, &a);
 
-        let registers = self.directories.read(&ap)?;
+        let registers = self.directories.read_locker(&ap)?;
 
         Ok(Resolve::Find(registers))
     }
@@ -128,84 +128,10 @@ impl Keeper {
 
         let path = DirManager::append_path(&e, &a);         
         
-        self.directories.remove(&path)?;
+        self.directories.remove_locker(&path)?;
 
         Ok(Resolve::Remove)
     }
-
-    // pub fn new() -> Keeper<'l, 'd, 'f> {
-    //     let mut config_path = dirs::home_dir().unwrap();
-    //     let mut locker_path = dirs::home_dir().unwrap();
-
-    //     locker_path.push(".rk");
-    //     config_path.push(".config/rk");
-
-    //     Keeper { 
-    //         lock: Locker::new(),
-    //         files: FileManager::new(config_path, locker_path),
-    //         directories: DirManager::new(config_path, locker_path),
-    //     }
-    // }
-
-    // // TODO: read file + decrypt and find account
-    // fn find(&self) {
-    //     let mut file = self.read_locker();
-    // }
-
-    // fn handle_input() -> String {
-    //     let mut input = String::new();
-
-    //     stdin()
-    //         .read_line(&mut input)
-    //         .expect("Failed to read user input");
-
-    //     input
-    // }
-
-    // pub fn read_locker(&self) -> String {
-    //     let mut contents = String::new();
-    //     
-    //     let locker_path = self.files.get_locker_path();
-    //     let mut file = FileManager::open(locker_path, None, FileAction::Read);
-
-    //     match file.read_to_string(&mut contents) {
-    //         Err(why) => panic!("Couldn't open file to read: {}", why),
-    //         Ok(_) => println!("\n::: Success reading locker :::\n"),
-    //     };
-    //    
-    //     contents
-    // }
-
-    // pub fn write_on(&self, contents: &mut String) {
-    //     let mut input = Keeper::handle_input();
-    //     let encrypted = self.lock.input_encryption(&mut input);
-
-    //     // TODO: decryption working, have to define a way
-    //     // to maintain key and iv to decrypt further values
-    //     self.lock.input_decryption(&encrypted);
-
-
-    //     let locker_path = self.files.get_locker_path();
-    //     let mut file = FileManager::open(locker_path, None, FileAction::Write);
-    //     
-    //     let new_register = format!("{}", encrypted.trim());
-
-    //     contents.push_str(
-    //         new_register.as_str()
-    //     );
-
-    //     match file.write_all(contents.as_bytes()) {
-    //         Err(why) => panic!("Couldn't write to locker: {}", why),
-    //         Ok(_) => println!("\n::: Success writing to locker :::\n")
-    //     } 
-    // }
-
-    // pub fn add_account(&mut self) {
-    //     let input = Keeper::handle_input();
-    //     let hash = self.lock.hash(input);
-
-    //     self.directories.create(&hash[..]);
-    // }
 }
 
 #[cfg(test)]
