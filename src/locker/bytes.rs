f6c47d73e49aaad54f792e1c61333a0aa4aeae98
dsbrgg <dsbrgg@gmail.com>
2020-01-22 22:45:27 +0000

    update bytes.rs

--- src/locker/bytes.rs
+++ src/locker/bytes.rs
@@ -1,28 +1,183 @@
-pub struct Bytes<S>(Option<S>);
+use rand::{Rng, OsRng};
 
-impl<S> Bytes<S> {
-    fn read(&self) -> &Option<S> { &self.0 }
-    fn alloc(&mut self, size: S) { self.0 = Some(size); }
+#[derive(Debug)]
+pub enum ByteSize {
+  U16,
+  U32,
+  U64
+}
+
+#[derive(Debug)]
+pub struct Bytes {
+    hex: String,
+    binary: Vec<u8>
+}
+
+impl Bytes {
+   
+    /* Initializer */
+
+    pub fn new(size: ByteSize) -> Bytes {
+        let binary = match size {
+            ByteSize::U16 => Bytes::random_u16(),
+            ByteSize::U32 => Bytes::random_u32(),
+            ByteSize::U64 => Bytes::random_u64(),
+        };
+
+        let hex = Bytes::bin_to_hex(binary.clone());
+
+        Bytes {
+            hex,
+            binary
+        }
+    }
+
+    pub fn from_bin(binary: Vec<u8>) -> Bytes {
+        let hex = Bytes::bin_to_hex(binary.clone());
+
+        Bytes {
+            hex,
+            binary
+        }
+    }
+
+    pub fn from_hex(hex: String) -> Bytes {
+        let binary = Bytes::hex_to_bin(hex.clone());
+
+        Bytes {
+            hex,
+            binary
+        }
+    }
+
+    /* Hex operations */
+    
+    pub fn hex(&self) -> String { self.hex.clone() }
+    fn alloc_hex(&mut self, hex: String) { 
+        self.hex = hex.clone();
+        self.binary = Bytes::hex_to_bin(hex);
+    }
+
+    /* Binary operations */
+    
+    pub fn raw(&self) -> Vec<u8> { self.binary.clone() }
+    fn alloc_raw(&mut self, binary: Vec<u8>) { 
+        self.binary = binary.clone();
+        self.hex = Bytes::bin_to_hex(binary);
+    }
+
+    /* Associated functions */
+
+    fn random_u16() -> Vec<u8> {
+        let mut rng = OsRng::new().ok().unwrap();
+        let mut random_bytes: [u8; 16] = [0; 16];
+
+        println!("{:?}", random_bytes);
+
+        rng.fill_bytes(&mut random_bytes);
+
+        random_bytes.to_vec()
+    }
+
+    fn random_u32() -> Vec<u8> {
+        let mut rng = OsRng::new().ok().unwrap();
+        let mut random_bytes: [u8; 32] = [0; 32];
+
+        rng.fill_bytes(&mut random_bytes); 
+
+        random_bytes.to_vec()
+    } 
+
+    fn random_u64() -> Vec<u8> {
+        let mut rng = OsRng::new().ok().unwrap();
+        let mut random_bytes: [u8; 64] = [0; 64];
+
+        rng.fill_bytes(&mut random_bytes);
+
+        random_bytes.to_vec()
+    }
+
+    fn bin_to_hex(bytes: Vec<u8>) -> String {
+        bytes
+            .iter()
+            .map(|byte| format!("{:02x}", byte))
+            .fold(String::from("0x"), |string, hx| format!("{}{}", string, hx))
+    }
+
+    fn hex_to_bin(hex: String) -> Vec<u8> {
+        if !hex.is_empty() {
+            return hex::decode(&hex[2..]).unwrap();
+        }
+
+        Vec::new()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn new() {
-        let byte = Some([0]);
-        let b = Bytes(byte);
+    use ByteSize::*;
 
-        assert_eq!(b.read().unwrap(), [0]);
+    #[test]
+    fn new_u16() {
+        let byte = Bytes::new(U16);
+
+        assert_eq!(byte.raw().len(), 16);
     }
 
     #[test]
-    fn alloc() {
-        let mut b = Bytes(Some([0]));
-        
-        b.alloc([9]);
+    fn new_u32() {
+        let byte = Bytes::new(U32);
 
-        assert_eq!(b.read().unwrap(), [9]);
+        assert_eq!(byte.raw().len(), 32);
+    }
+
+    #[test]
+    fn new_u64() {
+        let byte = Bytes::new(U64);
+
+        assert_eq!(byte.raw().len(), 64);
+    }
+
+    #[test]
+    fn from_bin() {
+        let vec = [0].to_vec();
+        let byte = Bytes::from_bin(vec);
+
+        assert_eq!(byte.raw(), [0]);
+        assert_eq!(byte.hex(), String::from("0x00"));
+    }
+
+    #[test]
+    fn alloc_raw() {
+        let vec = [0].to_vec();
+        let other_vec = [1].to_vec();
+
+        let mut byte = Bytes::from_bin(vec);
+       
+        assert_eq!(byte.raw(), [0]);
+        assert_eq!(byte.hex(), String::from("0x00"));
+
+        byte.alloc_raw(other_vec);
+
+        assert_eq!(byte.raw(), [1]);
+        assert_eq!(byte.hex(), String::from("0x01"));
+    }
+
+    #[test]
+    fn alloc_hex() {
+        let vec = [0].to_vec();
+        let hex = String::from("0x01");
+
+        let mut byte = Bytes::from_bin(vec);
+       
+        assert_eq!(byte.raw(), [0]);
+        assert_eq!(byte.hex(), String::from("0x00"));
+
+        byte.alloc_hex(hex);
+
+        assert_eq!(byte.raw(), [1]);
+        assert_eq!(byte.hex(), String::from("0x01"));
     }
 }
