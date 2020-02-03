68571df4f070eaa689c746c68f78ab62db409849
dsbrgg <dsbrgg@gmail.com>
2020-02-03 07:52:01 +0000

    update managers/mod.rs

--- src/managers/mod.rs
+++ src/managers/mod.rs
@@ -1,7 +1,9 @@
 mod manager;
 mod dir_manager;
 mod file_manager;
+mod manager_option;
 
 pub use manager::Manager;
 pub use dir_manager::DirManager;
 pub use file_manager::FileManager;
+pub use manager_option::ManagerOption;
