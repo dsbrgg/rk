2e47d4c54765e640454d3faf0bf5f70162e48814
dsbrgg <dsbrgg@gmail.com>
2020-02-07 08:33:29 +0000

    update selection.rs

--- src/cli/selection.rs
+++ src/cli/selection.rs
@@ -2,20 +2,19 @@ use std::path::{PathBuf};
 use dialoguer::{theme::ColorfulTheme, Select};
 
 pub fn select(found: Vec<PathBuf>) -> PathBuf {
-    if found.len() > 1 {
-        let options: Vec<&str> = found.iter()
-            .map(|pb| pb.file_name().unwrap().to_str().unwrap())
-            .collect();
+    if found.len() == 0 { return PathBuf::new(); }
+    if found.len() == 1 { return PathBuf::from(found[0].clone()); }
+    
+    let options: Vec<&str> = found.iter()
+        .map(|pb| pb.file_name().unwrap().to_str().unwrap())
+        .collect();
 
-        let selection = Select::with_theme(&ColorfulTheme::default())
-            .with_prompt("Pick account")
-            .default(0)
-            .items(&options[..])
-            .interact()
-            .unwrap();
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick account")
+        .default(0)
+        .items(&options[..])
+        .interact()
+        .unwrap();
 
-        return found[selection].clone();
-    }
-
-    PathBuf::new()
+    found[selection].clone()
 }
