b89d76f1fcfe35748312f5b5dfed28e387f6d86d
dsbrgg <dsbrgg@gmail.com>
2020-02-10 08:14:12 +0000

    update selection.rs signature

--- src/cli/selection.rs
+++ src/cli/selection.rs
@@ -1,9 +1,9 @@
 use std::path::PathBuf;
 use dialoguer::{theme::ColorfulTheme, Select};
 
-pub fn select(found: Vec<PathBuf>) -> PathBuf {
-    if found.len() == 0 { return PathBuf::new(); }
-    if found.len() == 1 { return PathBuf::from(found[0].clone()); }
+pub fn select(found: Vec<PathBuf>) -> Option<PathBuf> {
+    if found.len() == 0 { return None; }
+    if found.len() == 1 { return Some(PathBuf::from(found[0].clone())); }
     
     let options: Vec<&str> = found.iter()
         .map(|pb| pb.file_name().unwrap().to_str().unwrap())
@@ -16,5 +16,5 @@ pub fn select(found: Vec<PathBuf>) -> PathBuf {
         .interact()
         .unwrap();
 
-    found[selection].clone()
+    Some(found[selection].clone())
 }
