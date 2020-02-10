273f1e2a0bd3b6cda9cbae12bc0803e4b28719b7
dsbrgg <dsbrgg@gmail.com>
2020-02-10 08:13:30 +0000

    update args.rs with helpers

--- src/args.rs
+++ src/args.rs
@@ -8,6 +8,9 @@ pub struct Args {
 }
 
 impl Args {
+    
+    /* Initialisers */
+
     pub fn new(
         entity: Option<&str>,
         account: Option<&str>,
@@ -28,6 +31,22 @@ impl Args {
             account: acc,
             password: pwd 
         }
+    }
+
+    /* Methods */
+
+    pub fn has_all(&self) -> bool {
+        !self.entity.is_empty()
+        && !self.account.is_empty()
+        && !self.password.is_empty()
+    }
+
+    pub fn has_entity(&self) -> bool {
+        !self.entity.is_empty()
+    }
+
+    pub fn has_account(&self) -> bool {
+        !self.account.is_empty()
     }
 }
 
@@ -50,6 +69,71 @@ mod tests {
 
         assert_eq!(entity, args.entity);
         assert_eq!(account, args.account);
+    }
+
+    #[test]
+    fn has_all() {
+        let args = Args::new(
+            Some("entity"),
+            Some("account"),
+            Some("password")
+        );
+
+        let has_all = args.has_all();
+
+        assert_eq!(has_all, true);
+    }
+
+    #[test]
+    fn has_entity() {
+        let args = Args::new(
+            Some("entity"),
+            Some("account"),
+            Some("password")
+        );
+
+        let has_entity = args.has_entity();
+
+        assert_eq!(has_entity, true);
+    }
+
+    #[test]
+    fn no_entity() {
+        let args = Args::new(
+            None,
+            Some("account"),
+            Some("password")
+        );
+
+        let has_entity = args.has_entity();
+
+        assert_eq!(has_entity, false);
+    }
+
+    #[test]
+    fn has_account() {
+        let args = Args::new(
+            Some("entity"),
+            Some("account"),
+            Some("password")
+        );
+
+        let has_account = args.has_account();
+
+        assert_eq!(has_account, true);
+    }
+
+    #[test]
+    fn no_account() {
+        let args = Args::new(
+            Some("entity"),
+            None,
+            Some("password")
+        );
+
+        let has_account = args.has_account();
+
+        assert_eq!(has_account, false);
     }
 }
 
