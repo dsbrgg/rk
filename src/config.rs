ef8b436b4114526416b58ad273e4368f65e179a2
dsbrgg <dsbrgg@gmail.com>
2020-02-18 08:15:27 +0000

    update config.rs with yaml methods

--- src/config.rs
+++ src/config.rs
@@ -1,37 +1,67 @@
-use std::env;
-use std::fs::File;
-use std::io::Read;
 use std::collections::HashMap;
+
+use serde_yaml:: Value;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Config<'c> {
-    path: &'c str 
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+struct Config {
+    paths: HashMap<String, Value>
 }
 
-impl<'c> Config<'c> {
-    fn init() {
-        let mut yaml = String::new();
-        let mut current_dir = env::current_dir().unwrap();
-        
-        current_dir.push("settings.yml");
+impl Config {
 
-        let path = current_dir.as_path();
-        let mut config_file = File::open(path).unwrap();
+    /* Initialisers */
 
-        config_file.read_to_string(&mut yaml).unwrap();
+    fn from_yaml(yaml: &str) -> Result<Config, serde_yaml::Error> {
+        serde_yaml::from_str(yaml)
+    }
 
-        println!("{:?}", yaml);
+    /* Methods */
+
+    fn to_yaml(&self) -> Result<String, serde_yaml::Error> {
+        serde_yaml::to_string(self) 
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    
+    use std::env;
+    use std::fs::File;
+    use std::io::Read;
 
     #[test]
-    fn init() {
-        Config::init();
-        panic!(":)");
+    fn from_yaml() {
+        let mut yaml = String::new();
+        let mut current_dir = env::current_dir().unwrap();
+
+        current_dir.push("settings.yml");
+        
+        let path = current_dir.as_path();
+        let mut config_file = File::open(path).unwrap();
+
+        config_file.read_to_string(&mut yaml).unwrap();
+
+        let deserialized = Config::from_yaml(&yaml).unwrap();
+        let mut default_paths: HashMap<String, Value> = HashMap::new();
+
+        let index = String::from("index");
+        let config = String::from("config");
+        let locker = String::from("locker");
+
+        let index_value =  Value::String("".to_string());
+        let config_value = Value::String("".to_string());
+        let locker_value = Value::String("".to_string());
+        
+        default_paths.insert(index, index_value);
+        default_paths.insert(config, config_value);
+        default_paths.insert(locker, locker_value);
+
+        let default_config = Config {
+            paths: default_paths
+        };
+
+        assert_eq!(deserialized, default_config);
     }
 }
