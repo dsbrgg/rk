53009da7ae15e9186f4e5231555fc0f41390a46b
dsbrgg <dsbrgg@gmail.com>
2020-03-31 17:15:57 +0100

    update yaml index

--- src/yaml/index.rs
+++ src/yaml/index.rs
@@ -4,7 +4,10 @@ use serde_yaml::Value;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-pub struct Index(String, HashMap<String, Value>);
+pub struct Index {
+    entities: Vec<Value>,
+    accounts: HashMap<String, Value>
+}
 
 impl Index {
 
@@ -20,7 +23,48 @@ impl Index {
         serde_yaml::to_string(self) 
     }
 
-    pub fn get(&self, path: String) -> String {
+    pub fn add(&self, entity: String, account: String) {
+        
+    }
+
+    pub fn get(&self, entity: String, account: Option<String>) -> String {
        String::new() 
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn from_yaml() {
+        let mut accounts: HashMap<String, Value> = HashMap::new();
+        let entities = vec![Value::String("entity_hash".to_string())]; 
+        accounts.insert("account_hash".to_string(), Value::String("entity_hash".to_string()));
+
+        let yaml = "---\nentities:\n  - entity_hash\naccounts:\n  account_hash: entity_hash";
+        let index = Index::from_yaml(yaml).unwrap();
+
+        let compare = Index {
+            entities,
+            accounts
+        };
+
+        assert_eq!(index, compare);
+    }
+
+    #[test]
+    fn to_yaml() {
+        let mut accounts: HashMap<String, Value> = HashMap::new();
+        let entities = vec![Value::String("entity_hash".to_string())]; 
+        accounts.insert("account_hash".to_string(), Value::String("entity_hash".to_string()));
+        
+        let yaml = "---\nentities:\n  - entity_hash\naccounts:\n  account_hash: entity_hash";
+        let index = Index {
+            entities,
+            accounts
+        };
+
+        assert_eq!(index.to_yaml().unwrap(), yaml);
     }
 }
