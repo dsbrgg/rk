24206e63ba0d09b7f7cf079d34d500b9e704c5b2
dsbrgg <dsbrgg@gmail.com>
2020-05-12 12:23:14 +0100

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 #![allow(warnings)]
 
 mod app;
-mod cli;
 mod args;
+mod cli;
 mod locker;
 mod managers;
 mod mocks;
 mod yaml;
 
+use args::*;
+use cli::CLI;
+use yaml::{
+    Settings, 
+    SettingsOpts::*
+};
+
 // TODO:
 // extract cmd -> encrypt/zip all passwords and send it to a specific path
 // set config cmd -> allow for custom config settings (eg. locker location, config location, cypto algo, etc...)
 
-fn main() {
-    use cli::CLI;
-    use yaml::{
-        Settings, 
-        SettingsOpts::*
-    };
-
+fn main() { 
     let args = app::execute();
     let settings = Settings::default();
     
