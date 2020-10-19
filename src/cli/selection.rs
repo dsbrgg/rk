d50aec634d81d28de9dcac7b8e7930c6d8f1dcaa
dsbrgg <dsbrgg@gmail.com>
2020-10-19 18:04:00 +0100

    Update selection.rs

--- src/cli/selection.rs
+++ src/cli/selection.rs
@@ -4,18 +4,14 @@ use rk::Encrypted;
 
 use dialoguer::{theme::ColorfulTheme, Select};
 
-pub fn select(found: Vec<Encrypted>) -> Option<Encrypted> {
+pub fn select(found: Vec<String>) -> Option<String> {
     if found.len() == 0 { return None; }
     if found.len() == 1 { return Some(found[0].clone()); }
     
-    let options: Vec<String> = found.iter()
-        .map(|enc| enc.path())
-        .collect();
-
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Pick account")
         .default(0)
-        .items(&options[..])
+        .items(&found[..])
         .interact()
         .unwrap();
 
