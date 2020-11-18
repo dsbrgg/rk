3a92abf7772fd9829c157ae6fc309cbcddb13b98
dsbrgg <dsbrgg@gmail.com>
2020-11-18 18:19:08 +0000

    update tables/mod.rs

--- src/tables/mod.rs
+++ src/tables/mod.rs
@@ -1,5 +1,44 @@
-mod list;
-mod find;
+use std::io;
 
-pub use list::list_table;
-pub use find::find_table;
+use cli_table::{
+    format::{CellFormat},
+    Cell, 
+    Table, 
+    Row
+};
+
+pub fn list_table(list: Vec<String>, accounts: bool) -> io::Result<()> {
+    let bold = CellFormat::builder().bold(true).build();
+    let header = if !accounts { format!("Entity") } else { format!("Account") };
+    let mut rows = vec![
+        Row::new(vec![
+            Cell::new(&header, bold)
+        ])
+    ];
+
+    for register in list {
+        let row = Row::new(vec![Cell::new(&register, Default::default())]);
+
+        rows.push(row);
+    }
+
+    let table = Table::new(rows, Default::default()).unwrap();
+
+    table.print_stdout()
+}
+
+pub fn find_table(password: String) -> io::Result<()> {
+    let bold = CellFormat::builder().bold(true).build();
+    let rows = vec![
+        Row::new(vec![
+            Cell::new(&format!("Password"), bold),
+        ]),
+        Row::new(vec![
+            Cell::new(&password, Default::default())
+        ])
+    ];
+
+    let table = Table::new(rows, Default::default()).unwrap();
+
+    table.print_stdout()
+}
